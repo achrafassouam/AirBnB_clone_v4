0d21fd2dcf25017e5789222944d48c6fdf07a1f1
“Achraf <achrafassouam@gmail.com>
2024-05-31 23:39:22 +0100

    updating 0-hbnb.py by adding uuid and updating 0-hbnb.html by adding the variable cache_id as query string to each <link> tag URL

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -8,8 +8,7 @@ from models.place import Place
 from os import environ
 from flask import Flask, render_template
 app = Flask(__name__)
-# app.jinja_env.trim_blocks = True
-# app.jinja_env.lstrip_blocks = True
+import uuid
 
 
 @app.teardown_appcontext
@@ -34,10 +33,13 @@ def hbnb():
     places = storage.all(Place).values()
     places = sorted(places, key=lambda k: k.name)
 
-    return render_template('100-hbnb.html',
+    cache_id = uuid.uuid4()
+
+    return render_template('0-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
-                           places=places)
+                           places=places,
+                           cache_id=cache_id)
 
 
 if __name__ == "__main__":
